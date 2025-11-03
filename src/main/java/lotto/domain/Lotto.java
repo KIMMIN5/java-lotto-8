2ff3dbf6fb904ea6d93fa0f759cb8737092f4817
KIMMIN5 <alsdh1111@naver.com>
2025-11-03 13:58:05 +0900

    feat: 로또 객체 중복, 범위 검증

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        LottoRules.validateRangeOfLotto(numbers);
+        LottoRules.validateDuplication(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return List.copyOf(numbers);
+    }
+}
