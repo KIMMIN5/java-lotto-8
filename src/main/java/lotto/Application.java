907a35a57148e9389a2ef5653098bb620fc576b3
KIMMIN5 <alsdh1111@naver.com>
2025-11-03 20:02:39 +0900

    feat: main 조립 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.domain.*;
+import lotto.view.*;
+import lotto.controller.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Parser parser = new Parser();
+        LottoController lottoController = new LottoController(inputView, outputView, parser);
+        lottoController.run();
     }
 }
